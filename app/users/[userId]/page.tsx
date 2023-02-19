9194b3b28adac2fbdf95f1c2b70660fae7fac027
ErikRobles <erikjames69@hotmail.com>
2023-02-19 12:50:00 -0600

    added getUser function to page.tsx inside [userId] folder

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,13 +1,35 @@
 import React from "react"
-import { getUserById } from "../../../lib/prisma/users"
+import { getUserById, getUsers } from "../../../lib/prisma/users"
 import User from "./user"
+
+interface User {
+  id: string
+  name: string
+  email: string
+  imageUrl: string
+}
+
+export async function getStaticParams() {
+  const users: any = await getUsers()
+  return users.map((user: User) => ({
+    userId: user.id,
+  }))
+}
+
+async function getUser({ userId }: any) {
+  const { user }: any = await getUserById(userId)
+  if (!user) {
+    throw new Error("Failed to fetch Data.")
+  }
+  return user
+}
 
 type Props = {
   params: any
 }
 
 const Page = async ({ params }: Props) => {
-  const user = await getUserById(params.userId)
+  const user = await getUser(params.userId)
 
   return <User user={user} />
 }
